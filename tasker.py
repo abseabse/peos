d32ace5f50460299364738a38eb9d78eaaeac444
odinsapog <sevapan@gmail.com>
2017-11-24 19:00:48 +0500

    Adding an existing code

---
+++ tasker.py
@@ -0,0 +1,206 @@
+# Version: 3 
+# Date: 23.11.17
+# Time: 11:14
+
+
+
+# IMPORTS
+import sqlite3
+import sys
+
+# GLOBAL VARIABLES
+testmode = 1    # if value == 1, then test block will be executed, otherwise not
+quit = 1
+
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+c.execute('''pragma foreign_keys = on''')
+
+# CREATING TABLES
+c.execute('''CREATE TABLE IF NOT EXISTS notes (
+        ID_note integer primary key, 
+        note text)''')
+c.execute('''CREATE TABLE IF NOT EXISTS tags (
+        ID_tag integer primary key, 
+        tag text)''')
+c.execute('''CREATE TABLE IF NOT EXISTS notes_tags (
+        ID_note integer not null,
+        ID_tag integer not null,
+        foreign key (ID_note) references notes(ID_note),
+        foreign key (ID_tag) references tags(ID_tag),
+        primary key (ID_note, ID_tag))''')
+conn.commit()
+
+# TEST BLOCK FOR TABLES
+# test 1: table notes is functioning
+if testmode == 1:
+    try:
+        c.execute("insert into notes VALUES (1, 'gogakal')")
+        conn.commit()
+    except:
+        print('test 1 failed')
+    else:
+        c.execute("delete from notes")
+        conn.commit()
+        print('test 1 passed')
+
+# test 2: in table notes ID_note is a primary key
+if testmode == 1:
+    try:
+        c.execute("insert into notes VALUES(1, 'gogakal')")
+        c.execute("insert into notes VALUES(1, 'gogakal2')")
+        conn.commit()
+    except:
+        c.execute("delete from notes")
+        print('test 2 passed')
+    else:
+        c.execute("delete from notes")
+        conn.commit()
+        print('test 2 failed')
+
+# test 3: table tags is functioning
+if testmode == 1:
+    try:
+        c.execute("insert into tags VALUES (1, 'o kale')")
+        conn.commit()
+    except:
+        print('test 3 failed')
+    else:
+        c.execute("delete from tags")
+        conn.commit()
+        print('test 3 passed')
+
+# test 4: in table tags ID_tag is a primary key
+if testmode == 1:
+    try:
+        c.execute("insert into tags VALUES ('1, 'o kale')")
+        c.execute("insert into tags VALUES ('1, 'o fekale')")
+        conn.commit()
+    except:
+        print('test 4 passed')
+    else:
+        c.execute("delete from tags")
+        conn.commit()
+        print('test 4 failed')
+
+# test 5: in table notes_tags both attributes goes with not null constraint
+if testmode == 1:
+    try:
+        c.execute("""insert into notes_tags VALUES (null, null)""")
+        conn.commit()
+    except:
+        print('test 5 passed')
+    else:
+        c.execute("delete from notes_tags")
+        conn.commit()
+        print('test 5 failed')
+
+# test 6: table notes_tags is working in overall
+
+if testmode == 1:
+    try:
+        c.execute("""insert into notes VALUES (1, 'goga')""")
+        c.execute("""insert into tags VALUES (2, 'kal')""")
+        c.execute("""insert into notes_tags VALUES (1, 2)""")
+    except:
+        print('test 6 failed')
+    else:
+        c.execute("delete from notes_tags")
+        c.execute("delete from notes")
+        c.execute("delete from tags")
+        conn.commit()
+        print('test 6 passed')
+
+# test 7: in table notes_tags both attributes are foreign keys
+if testmode == 1:
+    try:
+        c.execute("""insert into notes_tags VALUES (1, 2)""")
+        conn.commit()
+    except:
+        print('test 7 passed')
+    else:
+        c.execute("delete from notes_tags")
+        conn.commit()
+        print('test 7 failed')
+
+# test 8: in table notes_tags both attributes form a complex primary key
+if testmode == 1:
+    try:
+        c.execute("""insert into notes VALUES (1, 'goga')""")
+        c.execute("""insert into tags VALUES (2, 'kal')""")
+        c.execute("""insert into notes_tags VALUES (1, 2)""")
+        c.execute("""insert into notes_tags VALUES (1, 2)""")
+        conn.commit()
+    except:
+        print('test 8 passed')
+    else:
+        c.execute("delete from notes_tags")
+        c.execute("delete from notes")
+        c.execute("delete from tags")
+        print('test 8 failed')
+        conn.commit()
+
+# test_last: clearing tables
+if testmode == 1:
+    c.execute("delete from notes_tags")
+    c.execute("delete from notes")
+    c.execute("delete from tags")
+    conn.commit()
+
+
+# FUNCTIONS
+def tasker_quit(ask=0):
+    """
+    1>>> tasker_quit(1)
+    1Are you sure to quit? [y] [n] \n
+    """
+    if ask == 1:
+        user_command = input('Are you sure to quit? [y] [n] \n')
+        if user_command == 'y':
+            sys.exit()
+    else:
+        sys.exit()
+
+def tasker_add(task):
+    #TODO delete the last execution where all the rows are deleted from notes
+    """
+    >>> tasker_add('tasker    gogakal ronyal iskal     #')
+    gogakal ronyal iskal
+    """
+    end_of_task = task.find('#')
+    task_name = task[7:end_of_task].strip()
+    c.execute("""insert into notes VALUES (Null, ?)""", (task_name,))
+    a = c.execute("""select * from notes""")
+    for row in a:
+        print(row[1])
+    c.execute("""delete from notes""")
+
+def return_tags(text):
+    """
+    >>> return_tags('goga, mnogo ,,  , kal iskal, ne')
+    ['goga', 'mnogo', 'kal iskal', 'ne']
+    """
+    initial_list = text.split(',')
+    return_list = []
+    for tag in initial_list:
+        if tag.isspace() == True or tag == '':
+            pass
+        else:
+            return_list.append(tag.strip())
+    return(return_list)
+        
+
+# TEST CYCLE
+if __name__ == '__main__':
+    if testmode == 1:
+        import doctest
+        doctest.testmod()
+
+
+# MAIN CYCLE
+    while quit == 1:
+        user_command = input('Enter command: ')
+        if user_command == 'tasker quit':
+            tasker_quit(ask=1)
+        else:
+            print('gogakal')
---
+++ testdoc.py
@@ -0,0 +1,17 @@
+"""
+>>> gogakal(1, 2)
+3
+"""
+
+
+def gogakal(a, b):
+    """
+    >>> gogakal (1, 3)
+    4
+    """
+    return a + b
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    
